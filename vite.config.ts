ca5e163fae3e1b26292b589790cc5e50cf7dcf8e
Athena-Pro <athenakaminsky@gmail.com>
2025-08-15 18:51:08 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './vitest.setup.ts',
-  },
-});
+  base: '/<your-repo-name>/', // 👈 must match repo name exactly
+})
